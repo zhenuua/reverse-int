ac7e6565a0d00f03318e0f8cb6d4d091116c0177
zhenuua <zhenuua@gmail.com>
2021-03-13 18:39:04 +0300

    feat: make function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let result = '';
+  n = Math.abs(n);
+  n = n.toString()
+  for(let i = n.length - 1; i >= 0; i--){
+    result += n[i];
+  }
+  return result;
 }
